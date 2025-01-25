84e99830f329e98700899e019df325296f0c41c6
harryfritz-dot <harryforrosuelo15@gmail.com>
2025-01-25 11:58:17 +0800

    added images

--- src/constant/index.js
+++ src/constant/index.js
@@ -7,46 +7,46 @@ export const ProjectList = [
     imgLink: "/projects/hpdb.png",
     color: "#4D99BC",
   },
-  // {
-  //     id: 2,
-  //     title: "OurHimitsu",
-  //     desk: "Platform for students to express thoughts and ideas in an inclusive online space.",
-  //     link: 'https://freedom-wall-v2.vercel.app/',
-  //     imgLink: '/projects/freedomWall.png',
-  //     color:"#EDA208"
-  // },
-  // {
-  //     id: 3,
-  //     title: "PalengkE",
-  //     desk: "E-commerce app connecting farmers and consumers for buying and selling fresh produce.",
-  //     link: '',
-  //     imgLink: '/projects/palengke.png',
-  //     color:"#435E39"
-  // },
-  // {
-  //     id: 4,
-  //     title: "Capstone Title Generator",
-  //     desk: "Generates unique project titles for tech students with a single click.",
-  //     link: 'https://zizaaa.github.io/Title-Capstone-Generator/',
-  //     imgLink: '/projects/capstoneGenerator.png',
-  //     color:"#1E203C"
-  // },
-  // {
-  //     id: 5,
-  //     title: "ChatVerse",
-  //     desk: "Messaging platform with features like anonymous messaging for global conversations.",
-  //     link: 'https://chat-verse-dev.vercel.app/',
-  //     imgLink: '/projects/chatverse.png',
-  //     color:"#A69988"
-  // },
-  // {
-  //     id: 6,
-  //     title: "WeTech Bot",
-  //     desk: "Represents collaboration and innovation in technology.",
-  //     link: '',
-  //     imgLink: '/projects/wetechBot.png',
-  //     color:"#23358D"
-  // },
+  {
+    id: 2,
+    title: "You Are Beautiful",
+    desk: "Platform for students to express thoughts and ideas of searching every images.",
+    link: "https://afritz-tech.github.io/ACTIVITY.PP3/",
+    imgLink: "/projects/pp3.png",
+    color: "#EDA208",
+  },
+  {
+    id: 3,
+    title: "Quotes",
+    desk: " typically refers to a detailed explanation or context given about a quote. This could include: Source, Meaning, Context and Impact.",
+    link: "https://afritz-tech.github.io/quotes.../",
+    imgLink: "/projects/qts.png",
+    color: "#435E39",
+  },
+  {
+    id: 4,
+    title: "X-MEN",
+    desk: "the team is centered around individuals who possess extraordinary abilities due to mutations in their genetic code, making them a distinct branch of humanity called mutants.",
+    link: "https://afritz-tech.github.io/XMEN-/",
+    imgLink: "/projects/xmen.png",
+    color: "#1E203C",
+  },
+  {
+    id: 5,
+    title: "PP2 Static Portfolio",
+    desk: " Is a type of portfolio website that is built using fixed content and does not require server-side processing or dynamic updates. It is often designed to showcase personal, professional, or creative work in a simple, fast, and efficient way. ",
+    link: "https://afritz-tech.github.io/ACTIVITY.-PORTFOLIO-PP2/",
+    imgLink: "/projects/pp2.png",
+    color: "#A69988",
+  },
+  {
+    id: 6,
+    title: "PP1 static Portfolio",
+    desk: "Represents simple and creative way of building static portfolio website.",
+    link: "",
+    imgLink: "/projects/pp1.png",
+    color: "#23358D",
+  },
   // {
   //     id: 7,
   //     title: "Ziza Theme",
